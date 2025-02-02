36b5a19861545366fb628ec33fcfccf61520c2ac
Satish Patil <syapatil0803@gmail.com>
2025-02-02 14:55:20 +0530

    Initial commit

---
+++ src/test/java/com/microservices/backend_client/BackendClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.backend_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
